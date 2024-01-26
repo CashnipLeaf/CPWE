8883ee96c9ee13ad2b9ff99ce9f330cdecd73bc7
CashnipLeaf <155596677+CashnipLeaf@users.noreply.github.com>
2024-01-26 12:16:00 -0800

    Add files via upload

--- CPWE/RegisterModularFI.cs
+++ CPWE/RegisterModularFI.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using ModularFI;
 using UnityEngine;
+using static VehiclePhysics.VPAudio;
 
 namespace CPWE
 {
@@ -19,7 +20,7 @@ namespace CPWE
                 Utils.LogInfo("Registering CPWE with ModularFlightIntegrator.");
                 try
                 {
-                    if (ModularFlightIntegrator.RegisterUpdateAerodynamicsOverride(AeroUpdate)) 
+                    if (ModularFlightIntegrator.RegisterUpdateAerodynamicsOverride(NewAeroUpdate)) 
                     { 
                         Utils.LogInfo("Successfully Registered CPWE's Aerodynamics Override with ModularFlightIntegrator."); 
                     }
@@ -36,56 +37,63 @@ namespace CPWE
             Destroy(this);
         }
 
-        //Newer aero update that should be more accurate and less taxing on the physics engine
-        void AeroUpdate(ModularFlightIntegrator fi, Part part)
+        void NewAeroUpdate(ModularFlightIntegrator fi, Part part)
         {
+            fi.BaseFIUpdateAerodynamics(part);
+
             CPWE_Core CPWEdata = CPWE_Core.Instance;
-            //Failsafes that will fall back to stock FI if anything goes wrong.
-            if (CPWEdata == null || part.staticPressureAtm <= 0.0)
-            {
-                fi.BaseFIUpdateAerodynamics(part);
-                return;
-            }
+            //Failsafes if the core failes to load.
+            if (CPWEdata == null) { return; }
+
             Vector3 windvec = CPWEdata.GetCachedWind() * Utils.GlobalWindSpeedMultiplier;
-            if(windvec == null || Utils.IsVectorNaNOrInfinity(windvec) || windvec == Vector3.zero)
-            {
-                fi.BaseFIUpdateAerodynamics(part);
-                return;
-            }
+            if (windvec == null || Utils.IsVectorNaNOrInfinity(windvec) || windvec == Vector3.zero) { return; }
 
-            Rigidbody rb = part.Rigidbody;
-            if(rb != null)
+            Rigidbody rb = part.rb;
+            if (rb && !part.ShieldedFromAirstream && part.staticPressureAtm > 0.0 && !(part.Rigidbody != rb && !PhysicsGlobals.ApplyDragToNonPhysicsParts))
             {
-                bool rigid = part.rb != null;
-                bool servorigid = part.servoRb != null;
-                part.aerodynamicArea = 0.0;
-                part.exposedArea = fi.BaseFICalculateAreaExposed(part);
-                part.dynamicPressurekPa = part.submergedDynamicPressurekPa = 0.0;
-                if (rigid && part.angularDragByFI)
-                {
-                    part.angularDrag = 0.0f;
-                }
-                if (servorigid)
-                {
-                    part.servoRb.angularDrag = 0.0f;
-                }
-                double submerged = Math.Max(Math.Min(part.submergedPortion, 1.0), 0.0);
+                Vector3 DragForce = Vector3.zero;
+                Vector3 LiftForce = Vector3.zero;
+                Vector3 DragForceWithWind = Vector3.zero;
+                Vector3 LiftForceWithWind = Vector3.zero;
 
-                //insert the wind vector as an offset to the drag vector used for calculations
-                Vector3 adjustedwindvec = Vector3.Lerp(windvec, Vector3.zero, (float)submerged);
-                Vector3 normaldragVector = rb.velocity + Krakensbane.GetFrameVelocity();
-                Vector3 winddragvector = normaldragVector - adjustedwindvec;
-                if(part == fi.Vessel.rootPart && fi.Vessel.speedOfSound > 0.0)
+                Vector3 NormalDragVector = rb.velocity + Krakensbane.GetFrameVelocity();
+                Vector3 WindDragVector = NormalDragVector - windvec;
+                float machNumber = 0.0f;
+                if(fi.Vessel.speedOfSound > 0.0f)
                 {
-                    fi.mach = fi.Vessel.mach = winddragvector.magnitude / fi.Vessel.speedOfSound;
+                    machNumber = NormalDragVector.magnitude / (float)fi.Vessel.speedOfSound;
                 }
-                part.dragVector = winddragvector;
+
+                //get body drag/lift force w/o wind
+                if (!part.DragCubes.None)
+                {
+                    DragForce += -part.dragVectorDir * part.dragScalar;
+                }
+                if (!part.hasLiftModule)
+                {
+                    LiftForce += Vector3.ProjectOnPlane(part.transform.rotation * (part.bodyLiftScalar * part.DragCubes.LiftForce), -part.dragVectorDir);
+                }
+                //compute wing lift/drag force w/o wind
+                foreach (var m in part.Modules)
+                {
+                    if (m is ModuleLiftingSurface wing)
+                    {
+                        double Q = part.dynamicPressurekPa * 1000.0;
+                        wing.SetupCoefficients(wing.part.dragVector, out Vector3 nVel, out Vector3 liftvector, out float liftDot, out float absdot);
+                        LiftForce += wing.GetLiftVector(liftvector, liftDot, absdot, Q, machNumber);
+                        if (wing.useInternalDragModel) { DragForce += wing.GetDragVector(nVel, absdot, Q); }
+                    }
+                }
+
+                //insert new drag vector
+                part.dragVector = WindDragVector;
                 part.dragVectorSqrMag = part.dragVector.sqrMagnitude;
-
-                bool vec = false;
-                if(part.dragVectorSqrMag != 0.0f) 
+                if (part == fi.Vessel.rootPart && fi.Vessel.speedOfSound > 0.0)
                 {
-                    vec = true;
+                    fi.mach = fi.Vessel.mach = WindDragVector.magnitude / fi.Vessel.speedOfSound;
+                }
+                if (part.dragVectorSqrMag != 0.0f)
+                {
                     part.dragVectorMag = Mathf.Sqrt(part.dragVectorSqrMag);
                     part.dragVectorDir = part.dragVector / part.dragVectorMag;
                     part.dragVectorDirLocal = -part.partTransform.InverseTransformDirection(part.dragVectorDir);
@@ -94,158 +102,53 @@ namespace CPWE
                 {
                     part.dragVectorMag = 0.0f;
                     part.dragVectorDir = Vector3.zero;
-                    part.dragVectorDirLocal = Vector3.zero; 
+                    part.dragVectorDirLocal = Vector3.zero;
                 }
                 part.dragScalar = 0.0f;
 
-                if (part.ShieldedFromAirstream || (part.atmDensity <= 0.0f && submerged <= 0.0f)) { return; }
-
+                //update dynamic pressure
+                part.dynamicPressurekPa = part.atmDensity * 0.0005 * part.dragVectorSqrMag;
+                
+                //get body drag/lift force with wind
                 if (!part.DragCubes.None)
                 {
                     part.DragCubes.SetDrag(part.dragVectorDirLocal, (float)fi.mach);
                 }
-
                 part.aerodynamicArea = fi.BaseFICalculateAerodynamicArea(part);
-                if (!(PhysicsGlobals.ApplyDrag && vec)) { return; }
-                if (rb != part.rb && !PhysicsGlobals.ApplyDragToNonPhysicsParts) { return; }
-
-                double notsubmerged = 1.0 - submerged;
-                bool partsubmerged = false;
-                double staticpress;
-                double OceanDensity = fi.CurrentMainBody.oceanDensity * 1000.0;
-
-                if (fi.CurrentMainBody.ocean && submerged > 0.0f)
-                {
-                    partsubmerged = true;
-                    double cacheOceanDensity = OceanDensity * PhysicsGlobals.BuoyancyWaterAngularDragScalar * part.waterAngularDragMultiplier;
-                    part.submergedDynamicPressurekPa = OceanDensity;
-                    staticpress = Utils.Lerp(part.staticPressureAtm, cacheOceanDensity, submerged);
-                }
-                else
-                {
-                    part.dynamicPressurekPa = part.atmDensity;
-                    staticpress = part.staticPressureAtm;
-                }
-
-                double dynamicpressV = 0.0005 * part.dragVectorSqrMag;
-                part.dynamicPressurekPa *= dynamicpressV;
-                part.submergedDynamicPressurekPa *= dynamicpressV;
-                if (rigid && part.angularDragByFI)
-                {
-                    double angularDragQ;
-                    double cachewaterpress = part.submergedDynamicPressurekPa * 0.0098692326671601278 * PhysicsGlobals.BuoyancyWaterAngularDragScalar * part.waterAngularDragMultiplier;
-                    if (partsubmerged)
-                    {
-                        angularDragQ = staticpress + Utils.Lerp(part.dynamicPressurekPa * 0.0098692326671601278, cachewaterpress, submerged);
-                    }
-                    else
-                    {
-                        angularDragQ = part.dynamicPressurekPa * 0.0098692326671601278;
-                    }
-                    angularDragQ = Math.Max(angularDragQ, 0.0);
-
-                    part.rb.angularDrag = part.angularDrag * (float)angularDragQ * PhysicsGlobals.AngularDragMultiplier;
-                    if (servorigid)
-                    {
-                        part.servoRb.angularDrag = part.rb.angularDrag;
-                    }
-                }
-                int mode = 0;
-                if (PhysicsGlobals.DragUsesAcceleration) { mode = 5; }
-
                 double drag = fi.BaseFICalculateDragValue(part) * fi.pseudoReDragMult;
-                if (!double.IsNaN(drag) && drag != 0.0)
+                if(!double.IsNaN(drag) && drag != 0.0)
                 {
-                    part.dragScalar = (float)(part.dynamicPressurekPa * drag * notsubmerged) * PhysicsGlobals.DragMultiplier;
-                    //inline version of ApplyAeroDrag()
-                    Vector3 position;
-                    if (rb != part.rb)
+                    part.dragScalar = (float)(drag * part.dynamicPressurekPa) * PhysicsGlobals.DragMultiplier;
+                    DragForceWithWind += -part.dragVectorDir * part.dragScalar;
+                }
+                if (!part.hasLiftModule)
+                {
+                    part.bodyLiftScalar = (float)(part.dynamicPressurekPa * part.bodyLiftMultiplier * (double)PhysicsGlobals.BodyLiftMultiplier * (double)PhysicsGlobals.BodyLiftCurve.liftMachCurve.Evaluate((float)fi.mach));
+                    LiftForceWithWind += Vector3.ProjectOnPlane(part.transform.rotation * (part.bodyLiftScalar * part.DragCubes.LiftForce), -part.dragVectorDir);
+                }
+
+                //compute wing lift/drag force w/ wind
+                foreach (var m in part.Modules)
+                {
+                    if (m is ModuleLiftingSurface wing)
                     {
-                        if (PhysicsGlobals.ApplyDragToNonPhysicsPartsAtParentCoM)
-                        {
-                            position = rb.worldCenterOfMass;
-                            goto add_drag;
-                        }
-                    }
-                    position = part.partTransform.TransformPoint(part.CoPOffset);
-                add_drag:
-                    rb.AddForceAtPosition(-part.dragVectorDir * part.dragScalar, position, (ForceMode)mode);
-                    goto ApplyLift;
-                }
-                part.dragScalar = 0.0f;
-
-            ApplyLift:
-                if(part.hasLiftModule) 
-                {
-                    if (adjustedwindvec != Vector3.zero)
-                    {
-                        //compute and add additional forces to wing parts since their lift/drag calculations are (annoyingly) done separately from the flightintegrator >:(
-                        foreach (var m in part.Modules)
-                        {
-                            if (m is ModuleLiftingSurface wing)
-                            {
-                                double QLiftWithWind = Utils.Lerp(wing.part.dynamicPressurekPa, wing.part.submergedDynamicPressurekPa * wing.part.submergedLiftScalar, submerged) * 1000.0;
-                                double QDragWithWind = QLiftWithWind;
-                                wing.SetupCoefficients(wing.part.dragVector, out Vector3 nVel, out Vector3 liftvector, out float liftDot, out float absdot);
-                                Vector3 LiftForce = wing.GetLiftVector(liftvector, liftDot, absdot, QLiftWithWind, (float)fi.mach);
-                                Vector3 DragForce = wing.GetDragVector(nVel, absdot, QDragWithWind);
-
-                                double QLiftNoWind = Utils.Lerp(staticpress * 0.0005 * normaldragVector.sqrMagnitude, OceanDensity * 0.0005 * normaldragVector.sqrMagnitude * wing.part.submergedLiftScalar, submerged) * 1000.0;
-                                double QDragNoWind = QLiftNoWind;
-                                double normalmach = 0.0;
-                                if (fi.Vessel.speedOfSound > 0.0)
-                                {
-                                    normalmach = normaldragVector.magnitude / fi.Vessel.speedOfSound;
-                                }
-                                wing.SetupCoefficients(normaldragVector, out Vector3 nVel2, out Vector3 liftvector2, out float liftDot2, out float absdot2);
-                                Vector3 LiftForceNoWind = wing.GetLiftVector(liftvector2, liftDot2, absdot2, QLiftNoWind, (float)normalmach);
-                                Vector3 DragForceNoWind = wing.GetDragVector(nVel2, absdot2, QDragNoWind);
-
-                                Vector3 AddedForce = LiftForce - LiftForceNoWind;
-                                if (wing.useInternalDragModel) { AddedForce += DragForce - DragForceNoWind; }
-
-                                //Adapted from FAR - apply a numerical control factor
-                                float numericalControlFactor = (float)(wing.part.rb.mass * wing.part.dragVector.magnitude * 0.67 / (AddedForce.magnitude * TimeWarp.fixedDeltaTime));
-                                wing.part.AddForce(AddedForce * Math.Min(numericalControlFactor, 1));
-                            }
-                        }
-                    }
-                    return; 
-                }
-
-                if (part.bodyLiftOnlyUnattachedLiftActual)
-                {
-                    if (part.bodyLiftOnlyProvider != null && part.bodyLiftOnlyProvider.IsLifting) { return; }
-                }
-                double LiftQ;
-                if (!partsubmerged)
-                {
-                    LiftQ = part.dynamicPressurekPa;
-                }
-                else
-                {
-                    LiftQ = Utils.Lerp(part.dynamicPressurekPa, part.submergedDynamicPressurekPa * part.submergedLiftScalar, submerged);
-                }
-                double liftScalar = LiftQ * part.bodyLiftMultiplier * (double)PhysicsGlobals.BodyLiftMultiplier * (double)PhysicsGlobals.BodyLiftCurve.liftMachCurve.Evaluate((float)fi.mach);
-                part.bodyLiftScalar = (float)liftScalar;
-                if (part.bodyLiftScalar == 0.0 || part.DragCubes.LiftForce == Vector3.zero || part.DragCubes.LiftForce.IsInvalid()) { return; }
-
-                //inline version of ApplyAeroLift()
-                Vector3 position1;
-                if (rb != part.rb)
-                {
-                    if (PhysicsGlobals.ApplyDragToNonPhysicsPartsAtParentCoM)
-                    {
-                        position1 = rb.worldCenterOfMass;
-                        goto add_lift;
+                        double Q2 = part.dynamicPressurekPa * 1000.0;
+                        wing.SetupCoefficients(WindDragVector, out Vector3 nVel, out Vector3 liftvector, out float liftDot, out float absdot);
+                        LiftForceWithWind += wing.GetLiftVector(liftvector, liftDot, absdot, Q2, (float)fi.mach);
+                        if (wing.useInternalDragModel) { DragForceWithWind += wing.GetDragVector(nVel, absdot, Q2); }
                     }
                 }
-                position1 = part.partTransform.TransformPoint(part.CoLOffset);
-            add_lift:
-                Vector3 vector = Vector3.ProjectOnPlane(part.transform.rotation * (part.bodyLiftScalar * part.DragCubes.LiftForce), -part.dragVectorDir);
-                part.bodyLiftLocalPosition = part.partTransform.InverseTransformPoint(position1);
-                part.bodyLiftLocalVector = part.partTransform.InverseTransformDirection(vector);
-                rb.AddForceAtPosition(vector, position1, (ForceMode)mode);
+
+                //compute the difference in lift/drag with and without wind
+                Vector3 AddedForce = (DragForceWithWind - DragForce) + (LiftForceWithWind - LiftForce);
+                if(!Utils.IsVectorNaNOrInfinity(AddedForce) && AddedForce != Vector3.zero)
+                {
+                    //Adapted from FAR - apply a numerical control factor
+                    float numericalControlFactor = (float)(part.rb.mass * part.dragVector.magnitude * 0.67 / (AddedForce.magnitude * TimeWarp.fixedDeltaTime));
+
+                    //add the extra force to the part.
+                    part.AddForce(AddedForce * Math.Min(numericalControlFactor, 1));
+                }
             }
         }
 
